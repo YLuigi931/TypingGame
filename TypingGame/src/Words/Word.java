d8d407d785e33d3130685b471f54a965bed0d225
Luigi <Luigi@THOR.attlocal.net>
2020-11-07 13:51:19 -0600

    First Commit from eclipse

---
+++ TypingGame/src/Words/Word.java
@@ -0,0 +1,22 @@
+package Words;
+
+
+class WordObj implements Comparable<WordObj> {
+	
+	String subject;
+	
+	WordObj(String subject){
+		this.subject = subject;
+	}
+
+//	getWord
+	
+	@Override
+	public int compareTo(WordObj o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+}
